c7919e786916c0d2e537b1c14486bba3943ae6c2
Soon Jae Kwon <kwonsjay@gmail.com>
2014-03-29 12:17:21 -0700

    initiate backbone

---
+++ app/assets/javascripts/bowalum.js
@@ -0,0 +1,13 @@
+window.Bowalum = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Bowalum.initialize();
+});
